cde466ddd4b8e1269ed8df317f75954e92cedca9
Trejon Stallsworth <50248674+Trejon@users.noreply.github.com>
2020-04-05 12:01:27 -0600

    Add files via upload

---
+++ src/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+  state = {
+    term: ''
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      term: e.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.onFormSubmit(this.state.term);
+  };
+
+
+  render() {
+    return(
+      <div className="search-bar ui segment">
+        <form onSubmit={this.handleSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+            type="text" value={this.state.term}
+            onChange={this.handleChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
---
+++ src/components/config.js
@@ -0,0 +1,3 @@
+export const config = {
+  MY_KEY : 'AIzaSyBfRBwj01pN--gcbbTE1WCG6r16gg1F1fo'
+}
